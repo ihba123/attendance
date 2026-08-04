5a2032d268b80ecc90822c4e70f7a3ef4c395174
Abhi <abhijithkttr@gmail.com>
2026-08-04 16:48:42 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const ASSETS_TO_CACHE = [
     './manifest.json',
     'https://cdn.tailwindcss.com',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-    'https://cdn.jsdelivr.net/npm/face-api.js@0.22.2/dist/face-api.min.js'
+    'https://cdn.jsdelivr.net/npm/face-api.js@0.22.2/dist/face-api.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
 ];
 
